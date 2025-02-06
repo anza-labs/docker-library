584b4340e52e537212bcd30efe057336018e3c53
Mateusz Urbanek <mateusz.urbanek.98@gmail.com>
2025-02-06 19:36:15 +0100

    feat: added version change for kine
    Signed-off-by: Mateusz Urbanek <mateusz.urbanek.98@gmail.com>

--- hack/version-change.sh
+++ hack/version-change.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-GITHUB_ENV="${1:?GITHUB_ENV \(arg 1\) is required}"
+set -e
+GITHUB_OUTPUT="${1:?GITHUB_OUTPUT \(arg 1\) is required}"
 
 # Get the list of changed version.txt files
 git diff --name-only HEAD~1 HEAD | grep -E '^library/.+/version.txt$' | while read -r file; do
     PROJECT=$(echo "$file" | cut -d'/' -f2)
     VERSION=$(cat "$file")
-    echo "project=$PROJECT" >> "$GITHUB_ENV"
-    echo "version=$VERSION" >> "$GITHUB_ENV"
+    echo "Found project=$PROJECT version=$VERSION"
+    echo "project=$PROJECT" >> "$GITHUB_OUTPUT"
+    echo "version=$VERSION" >> "$GITHUB_OUTPUT"
 done
